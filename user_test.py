61ddaa95c403394ad86b39c2cf5f30cd6dd9fcda
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 13:50:05 +0300

    initialize

--- user_test.py
+++ user_test.py
@@ -3,3 +3,14 @@ from os import uname_result
 import unittest
 from user import User
 
+class Testuser(unittest.TestCase):
+
+ def setUp(self):
+
+        self.new_user = User("Moureen","Cheruiyot","8005")
+
+def test_init(self):
+
+    self.assertEqual(self.new_user.user_name,"Moureen")
+
+    self.assertEqual(self.new_user.password,"8005")
