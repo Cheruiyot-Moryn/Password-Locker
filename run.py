6fbdaa6b61c9f463f7e572470291012869abdcbd
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 14:10:41 +0300

    add save user function

--- run.py
+++ run.py
@@ -10,3 +10,9 @@ def create_credential(identity, user_name, password):
 
     new_cred = Credential(identity, user_name,password)
     return new_cred
+
+def save_credential(credential):
+    '''
+    Function that saves user credentials
+    '''
+    credential.save_credential()
