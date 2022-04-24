89c5a6010fc4342d09cd009061846d7fbf8557c7
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 13:51:41 +0300

    add new data

--- user_test.py
+++ user_test.py
@@ -14,3 +14,15 @@ def test_init(self):
     self.assertEqual(self.new_user.user_name,"Moureen")
 
     self.assertEqual(self.new_user.password,"8005")
+
+def test_save_user(self):
+
+    self.new_contact.save_contact()
+    test_user = User("Test","user","0729321741","test@user.com")
+    test_user.save_user()
+
+    self.new_user.delete_user()
+    self.assertEqual(len(UserString.user_list,1))
+    
+if uname_result =='main':
+    unittest.main()        
