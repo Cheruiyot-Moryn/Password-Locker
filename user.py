8b9579b9461ac96f177e4ab1fef4c65282cbb32e
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 13:33:33 +0300

    initialize variables

--- user.py
+++ user.py
@@ -8,3 +8,12 @@ class User:
     '''
     users_list=[]
     password_list = []
+
+    def __init__(self, firstname, lastname, account_name, password):
+        '''
+        Initializing the variables for the list of user data
+        '''
+        self.firstname=firstname
+        self.lastname=lastname
+        self.account_name=account_name
+        self.password=password
