be755ef5f8137dff2a5fc041015930ba811ee392
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 14:13:07 +0300

    add function

--- run.py
+++ run.py
@@ -22,3 +22,35 @@ def authenticate(name,password):
     Function that authenticates for signing in
     '''
     return Credential.authenticate_credential(name,password)
+
+def create_data(firstname, lastname, account_name, password):
+    
+    new_user = User(firstname, lastname, account_name, password)
+    return new_user
+
+def save_account(data):
+  
+    data.save_account()
+
+def generate_password(length):
+    
+    return User.password_generator(length)
+
+def cred_data_exists(number):
+    
+    return Credential.cred_data_exists(number)
+
+def data_exists(number):
+    
+    return User.data_exists(number)
+
+def display_data(user_number,data_number):
+    
+    return User.display_data(user_number,data_number)
+
+def account_exist(name):
+    
+    return User.account_exist(name)
+
+def copy_password(number,count):
+    User.copy_password(number,count)
