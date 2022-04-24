aa4cc6eaa940d6a98347a5573cb59fdf1d71de7c
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 13:32:01 +0300

    add imports

---
+++ user.py
@@ -0,0 +1,10 @@
+import string,random
+import pyperclip
+
+
+class User:
+    '''
+    Class that is creates and save new objects of user acccounts data
+    '''
+    users_list=[]
+    password_list = []
