816f32058892a80eeea9e0d08d20d4edacace98c
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-04-24 14:08:47 +0300

    add imports

---
+++ run.py
@@ -0,0 +1,12 @@
+from ast import If
+import string,random,time
+from unittest.mock import _NameArgsKwargs
+from httplib2 import Credentials
+import pyperclip
+from credential import Credential
+from user import User
+
+def create_credential(identity, user_name, password):
+
+    new_cred = Credential(identity, user_name,password)
+    return new_cred
